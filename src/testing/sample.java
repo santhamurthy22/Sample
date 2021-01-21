77f6bc6cf528d3d0767348aa4949a1dfc318895e
santhamurthy22 <santhamurthy22@gmail.com>
2021-01-21 17:27:28 +0530

    done

--- src/testing/sample.java
+++ src/testing/sample.java
@@ -5,6 +5,11 @@ public class sample {
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
+		System.out.println("s");
+		System.out.println("s");
+		System.out.println("s");
+		System.out.println("s");
+		System.out.println("s");
 	}
 
 }
